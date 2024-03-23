f04a90009fc78fa5cd97514e8590809991f99761
hrudai2002 <hrudai2002@gmail.com>
2024-03-24 01:12:46 +0530

    hide password functionality added

--- frontend/src/screens/auth/auth.tsx
+++ frontend/src/screens/auth/auth.tsx
@@ -5,6 +5,7 @@ import { Text } from "../../../@generics/components/text"
 import { ProjectColors } from "../../../@generics/enums/colors";
 import { Button } from "../../../@generics/components/Button";
 import { useState } from "react";
+import { Ionicons } from '@expo/vector-icons';
 
 export const SignUp = (props: any) => {
     const {
@@ -13,7 +14,7 @@ export const SignUp = (props: any) => {
         control,
         formState: { errors }
     } = useForm();
-    const [sigIn, setSignIn] = useState<boolean>(true);
+    const [hidePassoword, setHidePassword] = useState<boolean>(true);
     return (
         <SafeAreaView edges={['top']} style={{ flex: 1, backgroundColor: ProjectColors.Primary }}>
             <View style={styles.heading}>
@@ -21,56 +22,66 @@ export const SignUp = (props: any) => {
                 <Text style={{ color: ProjectColors.Secondary, fontSize: 15 }}>Create a new account</Text>
             </View>
             <View style={styles.loginDetails}>
-                <View style={styles.email}>
-                    <Text fontWeight={700} style={{ fontSize: 16 }}>Full Name</Text>
-                    <Controller
-                        control={control}
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <TextInput
-                                style={styles.input}
-                                onBlur={onBlur}
-                                onChange={value => onChange(value)}
-                                value={value}
-                            />
-                        )}
-                        name="name"
-                        rules={{ required: true }}
-                    />
-                </View>
-                <View style={styles.email}>
-                    <Text fontWeight={700} style={{ fontSize: 16 }}>Email</Text>
-                    <Controller
-                        control={control}
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <TextInput
-                                style={styles.input}
-                                onBlur={onBlur}
-                                onChange={value => onChange(value)}
-                                value={value}
-                            />
-                        )}
-                        name="email"
-                        rules={{ required: true }}
-                    />
-                </View>
-                <View style={styles.password}>
-                    <Text fontWeight={700} style={{ fontSize: 16 }}>Password</Text>
-                    <Controller
-                        control={control}
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <TextInput
-                                style={styles.input}
-                                onBlur={onBlur}
-                                onChange={value => onChange(value)}
-                                value={value}
-                            />
-                        )}
-                        name="password"
-                        rules={{ required: true }}
-                    />
-                    <TouchableOpacity onPress={() => props.setSignIn((prev) => !prev)}>
-                        <Text style={styles.alreadyHaveAccount}>already have an account?</Text>
-                    </TouchableOpacity>
+                <View style={styles.inputFields}>
+                    <View style={styles.email}>
+                        <Text fontWeight={700} style={{ fontSize: 16 }}>Full Name</Text>
+                        <Controller
+                            control={control}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <TextInput
+                                    style={styles.input}
+                                    onBlur={onBlur}
+                                    onChange={value => onChange(value)}
+                                    value={value}
+                                />
+                            )}
+                            name="name"
+                            rules={{ required: true }}
+                        />
+                    </View>
+                    <View style={styles.email}>
+                        <Text fontWeight={700} style={{ fontSize: 16 }}>Email</Text>
+                        <Controller
+                            control={control}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <TextInput
+                                    style={styles.input}
+                                    onBlur={onBlur}
+                                    onChange={value => onChange(value)}
+                                    value={value}
+                                />
+                            )}
+                            name="email"
+                            rules={{ required: true }}
+                        />
+                    </View>
+                    <View style={styles.password}>
+                        <Text fontWeight={700} style={{ fontSize: 16 }}>Password</Text>
+                        <Controller
+                            control={control}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <View style={styles.passwordInputField}>
+                                    <TextInput
+                                        secureTextEntry={hidePassoword}
+                                        style={[styles.input, {flex: 0.88}]}
+                                        onBlur={onBlur}
+                                        onChange={value => onChange(value)}
+                                        value={value}
+                                    />
+                                    {
+                                        hidePassoword ? 
+                                        <Ionicons onPress={() => setHidePassword((prev) => !prev)} style={{ flex: 0.12, justifyContent: "center", alignItems: "center" }}  name="eye" size={24} color="black" /> :
+                                        <Ionicons onPress={() => setHidePassword((prev) => !prev)} style={{ flex: 0.12, justifyContent: "center", alignItems: "center" }} name="eye-off" size={24} color="black" />
+                                    }
+                                </View>
+                            )}
+                            name="password"
+                            rules={{ required: true }}
+                        />
+                        <TouchableOpacity onPress={() => props.setSignIn((prev) => !prev)}>
+                            <Text style={styles.alreadyHaveAccount}>already have an account?</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <Button
                     style={styles.btn}
@@ -92,7 +103,7 @@ export const SignIn = (props: any) => {
         control,
         formState: { errors }
     } = useForm();
-    const [sigIn, setSignIn] = useState<boolean>(true);
+    const [hidePassoword, setHidePassword] = useState<boolean>(true);
     return (
         <SafeAreaView edges={['top']} style={{ flex: 1, backgroundColor: ProjectColors.Primary }}>
             <View style={styles.heading}>
@@ -100,40 +111,50 @@ export const SignIn = (props: any) => {
                 <Text style={{ color: ProjectColors.Secondary, fontSize: 15 }}>Login to your account</Text>
             </View>
             <View style={styles.loginDetails}>
-                <View style={styles.email}>
-                    <Text fontWeight={700} style={{ fontSize: 16 }}>Email</Text>
-                    <Controller
-                        control={control}
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <TextInput
-                                style={styles.input}
-                                onBlur={onBlur}
-                                onChange={value => onChange(value)}
-                                value={value}
-                            />
-                        )}
-                        name="email"
-                        rules={{ required: true }}
-                    />
-                </View>
-                <View style={styles.password}>
-                    <Text fontWeight={700} style={{ fontSize: 16 }}>Password</Text>
-                    <Controller
-                        control={control}
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <TextInput
-                                style={styles.input}
-                                onBlur={onBlur}
-                                onChange={value => onChange(value)}
-                                value={value}
-                            />
-                        )}
-                        name="password"
-                        rules={{ required: true }}
-                    />
-                    <TouchableOpacity onPress={() => props.setSignIn((prev) => !prev)}>
-                        <Text style={styles.alreadyHaveAccount}>create a new account?</Text>
-                    </TouchableOpacity>
+                <View style={styles.inputFields}>
+                    <View style={styles.email}>
+                        <Text fontWeight={700} style={{ fontSize: 16 }}>Email</Text>
+                        <Controller
+                            control={control}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <TextInput
+                                    style={styles.input}
+                                    onBlur={onBlur}
+                                    onChange={value => onChange(value)}
+                                    value={value}
+                                />
+                            )}
+                            name="email"
+                            rules={{ required: true }}
+                        />
+                    </View>
+                    <View style={styles.password}>
+                        <Text fontWeight={700} style={{ fontSize: 16 }}>Password</Text>
+                        <Controller
+                            control={control}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <View style={styles.passwordInputField}>
+                                    <TextInput
+                                        secureTextEntry={hidePassoword}
+                                        style={[styles.input, { flex: 0.88 }]}
+                                        onBlur={onBlur}
+                                        onChange={value => onChange(value)}
+                                        value={value}
+                                    />
+                                    {
+                                        hidePassoword ?
+                                            <Ionicons onPress={() => setHidePassword((prev) => !prev)} style={{ flex: 0.12, justifyContent: "center", alignItems: "center" }} name="eye" size={24} color="black" /> :
+                                            <Ionicons onPress={() => setHidePassword((prev) => !prev)} style={{ flex: 0.12, justifyContent: "center", alignItems: "center" }} name="eye-off" size={24} color="black" />
+                                    }
+                                </View>
+                            )}
+                            name="password"
+                            rules={{ required: true }}
+                        />
+                        <TouchableOpacity onPress={() => props.setSignIn((prev) => !prev)}>
+                            <Text style={styles.alreadyHaveAccount}>create a new account?</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <Button
                     style={styles.btn}
@@ -187,6 +208,14 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         gap: 10
     },
+    passwordInputField: {
+        flexDirection: 'row',
+        alignItems: "center",
+        backgroundColor: ProjectColors.Grey,
+        height: 50,
+        width: '90%',
+        borderRadius: 10,
+    },
     btn: {
         opacity: 0.8, 
         width: '90%'
@@ -195,5 +224,8 @@ const styles = StyleSheet.create({
         color: ProjectColors.Primary,
         textAlign: 'right',
         paddingRight: '10%'
+    },
+    inputFields: {
+        gap: 20
     }
 })
