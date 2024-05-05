0afc8ad839e2d5d44d0aa471bd07cb0aae571ea2
hrudai2002 <hrudai2002@gmail.com>
2024-05-05 15:54:10 +0530

    auth middleware added

--- backend/routes/badminton.router.ts
+++ backend/routes/badminton.router.ts
@@ -4,9 +4,9 @@ import { authMiddleWare } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 router.get('/live', authMiddleWare, getLiveMatches);
-router.get('/finished', getFinishedMatches);
-router.get('/:matchId', getMatchDetails);
-router.post('/create', createMatch);
-router.put('/updatescore', updateScore);
+router.get('/finished', authMiddleWare, getFinishedMatches);
+router.get('/:matchId', authMiddleWare, getMatchDetails);
+router.post('/create', authMiddleWare, createMatch);
+router.put('/updatescore', authMiddleWare, updateScore);
 
 export default router; 
