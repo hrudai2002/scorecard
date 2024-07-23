fec9de868eb1b25058a7292b0a260ad0145f53b2
hrudai2002 <hrudai2002@gmail.com>
2024-07-24 00:49:10 +0530

    get tournament matches and move to live match controller added

--- backend/controllers/tournament.controller.ts
+++ backend/controllers/tournament.controller.ts
@@ -2,7 +2,7 @@ import { Types } from "mongoose";
 import { Team } from "../models/team.model";
 import { roundRobinSchedule } from "./helpers";
 import { Tournament } from "../models/tournament.model";
-import { TOURNAMENT_STATUS } from "../enum";
+import { MATCH_STATUS, TOURNAMENT_STATUS } from "../enum";
 import { MatchDetails } from "../models/match-details.model";
 
 /**
@@ -18,6 +18,77 @@ export const getAllTournaments = async (req, res) => {
         user = new Types.ObjectId(user);
         const allTournamentsData = await Tournament.find({ user }).lean();
         return res.json({ success: true, data: allTournamentsData });
+    } catch (error) {
+        return res.json({ success: false, error: error.message });
+    }
+}
+
+/**
+ * @Get - tournament/:id
+ * @desc - get all the tournament matches
+ */
+export const getTournamentMatches = async (req, res) => {
+    try {
+        let { user } = req.query; 
+        let { id } = req.params; 
+        if(!user || !id) {
+            throw new Error('Invalid Request!');
+        }
+        user = new Types.ObjectId(user); 
+        const matches = await MatchDetails.find({ tournament: id }).populate('teamA teamB').sort({ matchNo: 1 }).lean();
+        const result = matches.map((doc: any) => {
+            if(doc.status == MATCH_STATUS.LIVE) {
+                return {
+                    date: doc.date,
+                    status: doc.status,
+                    totalSets: doc.totalSets,
+                    teamA: {
+                        name: doc.teamA.name,
+                        score: doc.teamA.sets[doc.teamA.sets.length - 1].score
+                    },
+                    teamB: {
+                        name: doc.teamB.name,
+                        score: doc.teamB.sets[doc.teamB.sets.length - 1].score
+                    },
+                    currentSet: doc.teamA.sets.length,
+                    matchType: doc.gameType,
+                    matchNo: doc.matchNo,
+                    _id: doc._id
+                }
+            } else if(doc.status == MATCH_STATUS.COMPLETED) {
+                return {
+                    date: doc.date,
+                    status: doc.status,
+                    totalSets: doc.totalSets,
+                    teamA: {
+                        name: doc.teamA.name,
+                        score: doc.teamA.sets[doc.teamA.sets.length - 1].score,
+                        winner: doc.teamA.sets[doc.teamA.sets.length - 1].winner
+                    },
+                    teamB: {
+                        name: doc.teamB.name,
+                        score: doc.teamB.sets[doc.teamB.sets.length - 1].score,
+                        winner: doc.teamB.sets[doc.teamB.sets.length - 1].winner
+                    },
+                    matchNo: doc.matchNo,
+                    winner: doc?.winner?.name,
+                    currentSet: doc.teamA.sets.length,
+                    matchType: doc.gameType,
+                    _id: doc._id
+                }
+            } else  {
+                return {
+                    date: doc.date, 
+                    status: doc.status,
+                    teamA: doc.teamA, 
+                    teamB: doc.teamB, 
+                    matchNo: doc.matchNo,
+                    matchType: doc.gameType, 
+                    _id: doc._id
+                }
+            }
+        })
+        return res.json({ success: true, data: result });
     } catch (error) {
         return res.json({ success: false, error: error.message });
     }
@@ -88,5 +159,42 @@ export const createTournament = async (req, res) => {
 
     } catch (error) {
         return res.json({ success: false, error: error.message });
+    }
+}
+
+/**
+ * @Put - tournament/movetolive
+ * @desc - moves the match from ready to live
+ */
+export const moveMatchToLive = async (req, res) => {
+    try {
+        let { matchId, team } = req.body;
+        if(!matchId || !team) {
+            throw new Error('Invalid Request!');
+        }
+        const match = await MatchDetails.findOne({ _id: matchId }).lean();
+        await MatchDetails.updateOne({ _id: matchId }, {
+            $set: {
+                status: MATCH_STATUS.LIVE
+            }
+        });
+        await Team.updateOne({ _id: match.teamA }, {
+            $set: {
+                sets: [{
+                    score: 0,
+                    serve: team == match.teamA ? true : false
+                }],
+            }
+        });
+        await Team.updateOne({ _id: match.teamB }, {
+            $set: {
+                sets: [{
+                    score: 0,
+                    serve: team == match.teamB ? true : false
+                }],
+            }
+        });
+    } catch (error) {
+        return res.json({ success: false, error: error.message })
     }
 }
--- backend/routes/tournament.router.ts
+++ backend/routes/tournament.router.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { authMiddleWare } from '../middlewares/auth.middleware';
-import { createTournament, getAllTournaments } from '../controllers/tournament.controller';
+import { createTournament, getAllTournaments, getTournamentMatches, moveMatchToLive } from '../controllers/tournament.controller';
 
 const router = express.Router();
 router.get('', getAllTournaments);
+router.get('/:id', getTournamentMatches);
 router.post('/create', authMiddleWare, createTournament);
+router.put('/movetolive', authMiddleWare, moveMatchToLive);
 
 
 export default router;
