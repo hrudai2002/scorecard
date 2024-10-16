16f7d716a600f8c507dbf28c10bdfc6e8a2cc215
hrudai2002 <hrudai2002@gmail.com>
2024-10-16 23:13:05 +0530

    removed environments.ts

--- environments/environments.ts
+++
@@ -1,7 +0,0 @@
-const developmentEnvironment = {
-    apiUrl: 'https://aws-backend.sayihrudai.dev'
-    // apiUrl: 'http://192.168.1.7:5001'
-
-}
-
-export const environments = developmentEnvironment;
